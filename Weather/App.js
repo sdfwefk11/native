b327ef121c3591bdc18ba0b7ddf2d0cf091e84eb
sdfwefk11 <mknhsad3398@naver.com>
2023-01-25 03:33:02 +0900

    날씨 앱 프로젝트 시작

--- Weather/App.js
+++ Weather/App.js
@@ -1,15 +1,89 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Dimensions,
+  View,
+  Text,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [errorMsg, setErrorMsg] = useState(true);
+  const [forecast, setForcast] = useState([]);
+  const [city, setCity] = useState("Loading...");
+  const WEATHER_API_KEY = "99960dd689032ddafd0fad2517955588";
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      const location = await Location.reverseGeocodeAsync(
+        {
+          latitude,
+          longitude,
+        },
+        { useGoogleMaps: false }
+      );
+      setCity(location);
+      const response = await fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alert&appid=${WEATHER_API_KEY}&units=metric`
+      );
+      const json = await response.json();
+      setForcast(json.daily);
+    })();
+  }, []);
   return (
     <>
-      <StatusBar style="dark" />
-      <View style={{ flex: 1, flexDirection: "row" }}>
-        <View style={styles.style1}></View>
-        <View style={styles.style2}></View>
-        <View style={styles.style3}></View>
-        <View></View>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        <View style={styles.city}>
+          <Text style={styles.cityName1}>{city[0].region}</Text>
+          <Text style={styles.cityName2}>{city[0].district}</Text>
+        </View>
+        <ScrollView
+          pagingEnabled
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.weather}
+        >
+          {forecast.length === 0 ? (
+            <View style={styles.day}>
+              <ActivityIndicator color="#FF66FF" size="large" />
+            </View>
+          ) : (
+            forecast.map((item, index) => (
+              <View key={index} style={styles.day}>
+                <View style={styles.tempView}>
+                  <Text style={styles.temp}>
+                    {parseFloat(item.temp.day).toFixed(1)}
+                  </Text>
+                  <View style={styles.miniTempView}>
+                    <Text style={styles.miniTemp}>
+                      morning {parseFloat(item.temp.morn).toFixed(1)}
+                    </Text>
+                    <Text style={styles.miniTemp}>
+                      night {parseFloat(item.temp.night).toFixed(1)}
+                    </Text>
+                  </View>
+                </View>
+                <Text style={styles.weatherMain}>{item.weather[0].main}</Text>
+                <Text style={styles.weatherDescription}>
+                  {item.weather[0].description}
+                </Text>
+              </View>
+            ))
+          )}
+        </ScrollView>
       </View>
     </>
   );
@@ -18,20 +92,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black",
-    alignItems: "center",
+    backgroundColor: "#CBE7F1",
+  },
+  city: {
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
-  style1: {
-    flex: 1,
-    backgroundColor: "red",
+  cityName1: {
+    fontSize: 68,
+    fontWeight: "500",
   },
-  style2: {
-    flex: 1,
-    backgroundColor: "green",
+  cityName2: {
+    fontSize: 40,
+    fontWeight: "500",
   },
-  style3: {
-    flex: 1,
-    backgroundColor: "blue",
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  tempView: {
+    flexDirection: "row",
+  },
+  miniTempView: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    marginLeft: 10,
+  },
+  temp: {
+    marginTop: 100,
+    fontSize: 150,
+  },
+  miniTemp: {},
+
+  weatherMain: {
+    fontSize: 40,
+  },
+  weatherDescription: {
+    marginTop: 20,
   },
 });
